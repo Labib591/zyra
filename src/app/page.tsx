0b9cbf3af0e21af6cfbaa2ecded5642478dc7108
Mahir Mohammed Labib <mahi.labib5@gmail.com>
2025-10-13 01:37:29 -0400

    Added routing to my canvases if logged in already

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 "use client";
 import Homepage from "@/components/homepage/Homepage";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <Homepage />;
+
+  const {data: session, status} = useSession();
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (status === "authenticated" && session?.user?.id) {
+      router.push(`/myCanvases/${session.user.id}`);
+    }
+  }, [status, session, router]);
+  
+  return <Homepage />;  
 }
